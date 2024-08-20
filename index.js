e6922a9e962dcafe2c39fb3d50db4a0b4a5c2e3d
Mbalentlediko <dikombalentle9@gmail.com>
2024-08-20 15:41:34 +0200

    Fixing an error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import {userRouter,express} from './c';
+import {userRouter,express} from './controller/userController.js';
 import {productRouter} from './Controller/productController.js';
-import cookies from 'cookie-parser';
 import {errorHandling} from './Middleware/errorHandling.js';
 import path from 'path';
 import { config } from 'dotenv';
@@ -10,15 +9,15 @@ config()
 const app = express()
 const port = +process.env.PORT || 3300
 
-// app.use((req, res, next)=>{
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header("Access-Control-Allow-Credentials","true");
-//     res.header("Access-Control-Allow-Methods","*");
-//     res.header("Access-Control-Request-Methods","*");
-//     res.header("Access-Control-Allow-Headers","*");
-//     res.header("Access-Control-Expose-Headers","Authorization");
-// next()
-// })
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Credentials","true");
+    res.header("Access-Control-Allow-Methods","*");
+    res.header("Access-Control-Request-Methods","*");
+    res.header("Access-Control-Allow-Headers","*");
+    res.header("Access-Control-Expose-Headers","Authorization");
+next()
+})
 // Middleware
 app.use(
     express.static('./static'),
